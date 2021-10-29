837e898bd4009f90a02a1e8026bf345f18984aec
Rene Sorger <rene_sorger@gmx.at>
2021-10-29 17:45:06 +0200

    Added Basic Navigation Capabilites per GPS and Autopilot

--- GPS_Direction_Logic.py
+++ GPS_Direction_Logic.py
@@ -9,49 +9,99 @@ from Data_Reader_Library import DataReader
 from Data_Writer_Library import DataWriter
 from HightController import AltitudeController
 import CustomExceptions
-
+from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+import socket
+from geopy.distance import distance
 # Merken, 1ster Wert = Y, 2ter Wert = x für GPS
 
 
 class GPS_Direction_Logic:
-    def __init__(
-        self, offset: float, dr: DataReader, dw: DataWriter, hc: AltitudeController
-    ):
-        self.Reader = dr
-        self.Writer = dw
-        self.target_fp = open("Target.txt", "r")
-        self.__TargetPosition = self.update_target()
-        self.__Offset = offset
-        self.Altitude = hc
-        self.Reader.read_positional_data()
-        self.Writer.send_positional_data()
-        self.Active = True
-        time.sleep(1)
+
+    def __init__(self, connection_string, offset, target_alt ):
+        self.current_position = []
+        self.target_alt = target_alt
+        self.offset = offset
+        self.target_position = []
+        try:
+            self.vehicle = connect(connection_string, heartbeat_timeout=15, wait_ready=True)
+
+        # Bad TCP connection
+        except socket.error as e:
+            raise e
+
+        # Bad TTY connection
+        except OSError as e:
+            raise e
+
+        # API Error
+        except APIException as e:
+            raise e
+        # Other error
+        except:
+            raise RuntimeError("Something Happened")
+        while not self.vehicle.is_armable:
+            print(" Waiting for vehicle to initialise...")
+
+            time.sleep(10)
+        self.current_target = []
+        self.load_targets()
+
+        self.__arm_drone()
+        self.active = True
 
     # get the new target from Update_target
+    # def update_target(self):
+    #     try:
+    #         current_position_string = self.target_fp.readline()
+    #         print("TargetPos ", current_position_string)
+    #         x_local_position, y_local_position = current_position_string.split(",")
+    #         print("Targetx", x_local_position, "Targety", y_local_position)
+    #         return_value = [float(x_local_position), float(y_local_position)]
+    #         self.__Angle_to_target = 0
+    #         return return_value
+    #     except IOError as e:
+    #         print("error")
+    #         if e.errno == errno.EACCES:
+    #             return "some default data"
+    #         raise e
+
+    def load_targets(self):
+
+        target_fp = open("Target.txt", "r")
+        target_text = target_fp.read()
+        target_text = target_text.splitlines()
+        for position in target_text:
+            tmp_positions = position.split(",")
+            self.target_position.append(tmp_positions)
+        print(self.target_position)
+        return
+
     def update_target(self):
-        try:
-            current_position_string = self.target_fp.readline()
-            print("TargetPos ", current_position_string)
-            x_local_position, y_local_position = current_position_string.split(",")
-            print("Targetx", x_local_position, "Targety", y_local_position)
-            return_value = [float(x_local_position), float(y_local_position)]
-            self.__Angle_to_target = 0
-            return return_value
-        except IOError as e:
-            print("error")
-            if e.errno == errno.EACCES:
-                return "some default data"
-            raise e
+        if len(self.target_position) == 0:
+            self.active = False
+            return
+        self.current_target = self.target_position[0]
+        self.target_position.pop(0)
+
 
     def update_position(self):
+
         try:
-            self.__Current_Position = self.Reader.return_positional_data()
-        except IOError as e:
-            print("error")
-            if e.errno == errno.EACCES:
-                return "some default data"
-            raise e
+            self.current_position = []
+            locationstring = self.vehicle.location.global_relative_frame
+            self.current_position.append(locationstring.lat)
+            self.current_position.append(locationstring.lon)
+            return
+
+        except:
+            raise RuntimeError()
+        # try:
+        #     self.__Current_Position = self.Reader.return_positional_data()
+        # except IOError as e:
+        #     print("error")
+        #     if e.errno == errno.EACCES:
+        #         return "some default data"
+        #     raise e
 
     def vector_length(self, vector):
         try:
@@ -99,71 +149,98 @@ class GPS_Direction_Logic:
     #         raise
 
     # general program object
-    Active = 1
+    active = 1
     GPS_FP = 0
 
-    # initiate Vectors for movement
-    __Current_Position = [0, 0]
-    __TargetPosition = []
+
+
     __Movement = []  # Movementvector to reach target
-    __Angle_to_target = 0
 
-    # when the x, y Current Pos coordinates are close enough so target+offset>pos>target-offset
-    __Offset = 0.2
 
-    def thread_wrapper(self):
-        while self.Active:
-            self.main_logic()
-        else:
-            self.Writer.Active = False
-            self.Reader.Active = False
-            self.Altitude.Active = False
+
+    def __arm_drone(self):
+        print("Arming motors")
+        self.vehicle.airspeed = 3
+        # Copter should arm in GUIDED mode
+        self.vehicle.mode = VehicleMode("GUIDED")
+        self.vehicle.armed = True
+        while not self.vehicle.armed:
+            print(" Waiting for arming...")
+
+            time.sleep(0.1)
+
+    def reach_operation_height(self):
+        while True:
+            alt = float(self.vehicle.location.global_relative_frame.alt)
+            if alt >= self.target_alt * 0.95:
+                print(alt)
+                print("Reached target altitude")
+                break
+
+        return
 
     def main_logic(self):
         try:
-            self.update_position()
-            self.plot_course()
+            self.vehicle.simple_takeoff(self.target_alt)
+            self.reach_operation_height()
 
-            """if len(self.__Current_Position) < 2:
-                raise 
-            """
 
-            if self.__Angle_to_target == -1:
-                self.__TargetPosition = self.update_target()
-                self.__Angle_to_target = 0
-                if self.__TargetPosition[0] > 361:
-                    self.Active = False
-            else:
-                # print(self.__Angle_to_target)
-                self.Writer.set_positon(self.__Angle_to_target)
+            while True:
+                self.update_target()
+                self.update_position()
+                #print(self.current_target)
+                if not self.active:
+                    break
+                point = LocationGlobalRelative(float(self.current_target[0]), float(self.current_target[1]),
+                                               float(self.target_alt))
+                self.vehicle.simple_goto(point)
+                while True:
+                    time.sleep(1)
+                    self.update_position()
+                    print(self.current_position)
+                    if self.__is_within_offset():
+                        break
 
-        except errno:
-            print(errno)
-            raise errno
 
-    def display_plot(self):
-        d = {
-            "Latitude": [self.__Current_Position[0], self.__TargetPosition[0]],
-            "Longitude": [self.__Current_Position[1], self.__TargetPosition[1]],
-        }
-        df = pd.DataFrame(data=d)
+        except APIException:
+            raise APIException
+    def __is_within_offset(self):
+        coords_1 = (self.current_target[0], self.current_target[1])
+        coords_2 = (self.current_position[0], self.current_position[1])
 
-        # Plot Graph for easier Display
-        BBox = (
-            min(d["Longitude"]) - 0.002,
-            max(d["Longitude"]) + 0.002,
-            min(d["Latitude"]) - 0.002,
-            max(d["Latitude"]) + 0.002,
-        )
-        city_map = plt.imread("map.jpg")
-        fig, ax = plt.subplots(figsize=(8, 7))
-        # print(df)
-        ax.scatter(df.Longitude, df.Latitude, s=10, c="red", alpha=1)
-        ax.set_title("Waypoints Where we need to go")
-        ax.set_xlim(BBox[0], BBox[1])
-        ax.set_ylim(BBox[2], BBox[3])
-        ax.imshow(city_map, zorder=0, extent=BBox)
-        plt.show()
+        dist = distance(coords_1, coords_2).km*1000
+        print("Distance in meters" + str(dist))
+
+        if dist < self.offset:
+            return True
+        else:
+            return False
+
+
+        return
+    # def display_plot(self):
+    #     d = {
+    #         "Latitude": [self.__Current_Position[0], self.__TargetPosition[0]],
+    #         "Longitude": [self.__Current_Position[1], self.__TargetPosition[1]],
+    #     }
+    #     df = pd.DataFrame(data=d)
+    #
+    #     # Plot Graph for easier Display
+    #     BBox = (
+    #         min(d["Longitude"]) - 0.002,
+    #         max(d["Longitude"]) + 0.002,
+    #         min(d["Latitude"]) - 0.002,
+    #         max(d["Latitude"]) + 0.002,
+    #     )
+    #     city_map = plt.imread("map.jpg")
+    #     fig, ax = plt.subplots(figsize=(8, 7))
+    #     # print(df)
+    #     ax.scatter(df.Longitude, df.Latitude, s=10, c="red", alpha=1)
+    #     ax.set_title("Waypoints Where we need to go")
+    #     ax.set_xlim(BBox[0], BBox[1])
+    #     ax.set_ylim(BBox[2], BBox[3])
+    #     ax.imshow(city_map, zorder=0, extent=BBox)
+    #     plt.show()
 
     def plot_course(self, should_display=False):
 
@@ -199,7 +276,7 @@ class GPS_Direction_Logic:
             print(errno)
         if should_display:
             self.display_plot()
-        # self.Active = 0
+        # self.active = 0
         if movement_length < self.__Offset:
             self.__Angle_to_target = -1
         # print("Angle: ", self.__Angle_to_target)
--- main.py
+++ main.py
@@ -4,34 +4,10 @@ import Data_Reader_Library
 import Data_Writer_Library
 import HightController
 from dronekit import connect
+from dronekit import connect, VehicleMode, LocationGlobalRelative, APIException
+import time
 
 
-# Create UDP socket to use for sending (and receiving)
+A = GPS_Direction_Logic.GPS_Direction_Logic('tcp:127.0.0.1:5760', offset=0.5, target_alt=40)
+A.main_logic()
 
-vehicle = connect('tcp:127.0.0.1:5760', wait_ready=True)
-print(vehicle.location.global_relative_frame)
-exit()
-f = open("GPS_Interface.txt", "r")
-sock = U.UdpComms(
-    udpIP="127.0.0.1", portTX=8000, portRX=8001, enableRX=True, suppressWarnings=False
-)
-Datareader = Data_Reader_Library.DataReader(Data_Reader_Library.read_from_udp, sock)
-Datawriter = Data_Writer_Library.DataWriter(Data_Writer_Library.send_to_UDP, sock)
-Hightcontroller = HightController.AltitudeController(3)
-A = GPS_Direction_Logic.GPS_Direction_Logic(3, Datareader, Datawriter, Hightcontroller)
-A.thread_wrapper()
-print(A.plot_course())
-# while True:
-#     sentdata = A.plot_course()
-#     sock.SendData(str(sentdata)) # Send this string to other application
-#
-#     data = sock.ReadReceivedData() # read data
-#
-#     if data != None: # if NEW data has been received since last ReadReceivedData function call
-#         print(data) # print new received data
-#         tmp = data.split(",")
-#         tmp2 = []
-#         for x in range(0,1):
-#             tmp2.append(float(tmp[x]))
-#         A.update_position(tmp2)
-#     time.sleep(1)
