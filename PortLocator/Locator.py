2381420363cc2b71bb8d33296839fafa67cb88f0
Rene Sorger <rene_sorger@gmx.at>
2022-01-14 13:05:49 +0100

    Portlocator Update

--- PortLocator/Locator.py
+++ PortLocator/Locator.py
@@ -12,6 +12,9 @@ for file in FileGlob:
         # Connect to the Vehicle (in this case a UDP endpoint)
         vehicle = connect(file, wait_ready=True, baud=57600, heartbeat_timeout=10)
         print("HEARTHBEAT FROM " + file)
+        print("GPS LOCATION:")
+        print(vehicle.location.global_relative_frame)
+
         exit()
     except:
         print("NO HEARTHBEAT IN 10 SECONDS FROM" + file)
